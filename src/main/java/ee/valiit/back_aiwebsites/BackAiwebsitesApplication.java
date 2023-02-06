8538927d1a3755885322e06e4a2ad6e3a812576d
Valter Aro <valteraro@gmail.com>
2023-02-06 13:45:37 +0200

    Initial commit

---
+++ src/main/java/ee/valiit/back_aiwebsites/BackAiwebsitesApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.back_aiwebsites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAiwebsitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAiwebsitesApplication.class, args);
+	}
+
+}
